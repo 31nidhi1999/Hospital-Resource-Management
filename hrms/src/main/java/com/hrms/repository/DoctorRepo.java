f48cf3b0352c961a720ad8182c88958a061a9cc4
Nidhi Nayak <nidhinayak@Nidhis-Mac-mini.local>
2025-11-10 09:57:21 +0530

    Part II Repository

--- hrms/src/main/java/com/hrms/repository/DoctorRepo.java
+++ hrms/src/main/java/com/hrms/repository/DoctorRepo.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.hrms.entity.Doctor;
+import java.util.List;
+
 
 @Repository
 public interface DoctorRepo extends JpaRepository<Doctor, Long> {
-
+	Doctor findByEmail(String email);
 }
--- hrms/src/main/java/com/hrms/repository/PatientRepo.java
+++ hrms/src/main/java/com/hrms/repository/PatientRepo.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.hrms.entity.Patient;
+import java.util.List;
+
 
 @Repository
 public interface PatientRepo extends JpaRepository<Patient, Long> {
-
+	Patient findByEmail(String email);
 }
